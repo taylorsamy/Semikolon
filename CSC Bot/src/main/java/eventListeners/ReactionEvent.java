8ed26364283d0ef2b0863fa0989ddb5bd813bf2f
Taylor Amy <taylorsamy@users.noreply.github.com>
2019-01-19 01:06:09 -0700

    Role adding/removing finished. Roles command finished.
    Signed-off-by: Taylor <taylor.amy101@gmail.com>

--- CSC Bot/src/main/java/eventListeners/ReactionEvent.java
+++ CSC Bot/src/main/java/eventListeners/ReactionEvent.java
@@ -10,10 +10,10 @@ import net.dv8tion.jda.core.managers.GuildController;
 public class ReactionEvent extends ListenerAdapter {
 
     private String messageID;
+
     public ReactionEvent(String messageID) {
         this.messageID = messageID;
     }
-
 
     @Override
     public void onMessageReactionAdd(MessageReactionAddEvent event) {
@@ -22,24 +22,26 @@ public class ReactionEvent extends ListenerAdapter {
         GuildController guild = new GuildController(event.getGuild());
         if (event.getMessageId().equals(messageID)) {
 
-            if (emote.isEmote()) {
-                switch (emote.getName()) {
-                    case "1501":
-                        Role comp1501 = event.getGuild().getRoleById(535910836351401985L);
-                        guild.addRolesToMember(event.getMember(), comp1501).queue();
-                        break;
-                    case "1502":
-                        Role comp1502 = event.getGuild().getRoleById(536081237740093441L);
-                        guild.addRolesToMember(event.getMember(), comp1502).queue();
-                        break;
-                    case "2511":
-                        Role comp2511 = event.getGuild().getRoleById(536081302613655553L);
-                        guild.addRolesToMember(event.getMember(), comp2511).queue();
-                        break;
-                    case "1505":
-                        Role math1505 = event.getGuild().getRoleById(536081372582772745L);
-                        guild.addRolesToMember(event.getMember(), math1505).queue();
-                        break;
+            if (!event.getUser().isBot()) {
+                if (emote.isEmote()) {
+                    switch (emote.getName()) {
+                        case "1501":
+                            Role comp1501 = event.getGuild().getRoleById(535910836351401985L);
+                            guild.addRolesToMember(event.getMember(), comp1501).queue();
+                            break;
+                        case "1502":
+                            Role comp1502 = event.getGuild().getRoleById(536081237740093441L);
+                            guild.addRolesToMember(event.getMember(), comp1502).queue();
+                            break;
+                        case "2511":
+                            Role comp2511 = event.getGuild().getRoleById(536081302613655553L);
+                            guild.addRolesToMember(event.getMember(), comp2511).queue();
+                            break;
+                        case "1505":
+                            Role math1505 = event.getGuild().getRoleById(536081372582772745L);
+                            guild.addRolesToMember(event.getMember(), math1505).queue();
+                            break;
+                    }
                 }
             }
         }
