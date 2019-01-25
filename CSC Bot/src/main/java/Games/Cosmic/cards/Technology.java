6be76927aaadd55e84d5a6f64a818623883dbfe3
Nahuel Paladino <43184105+npala043@users.noreply.github.com>
2019-01-24 19:19:16 -0700

    Updated tech
    added researchNum instance var
    updated constructor to initialize researchNum
    added getResearchNum()

--- CSC Bot/src/main/java/Games/Cosmic/cards/Technology.java
+++ CSC Bot/src/main/java/Games/Cosmic/cards/Technology.java
@@ -1,4 +1,15 @@
 package games.cosmic.cards;
 
 public class Technology extends Card {
+  
+  private int researchNum;
+  
+  public Technology(int researchNum) {
+    this.researchNum = researchNum;
+  }
+  
+  public int getResearchNum() {
+    return this.researchNum; 
+  }
+  
 }
