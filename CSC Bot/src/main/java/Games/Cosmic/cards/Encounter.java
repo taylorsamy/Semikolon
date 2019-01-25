32e9a48afe8e58456247ec01258ad52f1d447917
Nahuel Paladino <43184105+npala043@users.noreply.github.com>
2019-01-24 19:36:34 -0700

    refactored encounter to include all 3 types of encounter

--- CSC Bot/src/main/java/Games/Cosmic/cards/Encounter.java
+++ CSC Bot/src/main/java/Games/Cosmic/cards/Encounter.java
@@ -1,4 +1,27 @@
 package games.cosmic.cards;
 
 public class Encounter extends Card {
+
+  public enum Type{ATTACK, NEGOTIATE, MORPH}
+  
+  private int atkValue;
+  private Type encounterType;
+  
+  public Encounter(Type encounterType) {
+    this.encounterType = encounterType;
+    this.atkValue = 0;
+  }
+  
+  public int getAttack() {
+    return this.atkValue; 
+  }
+  
+  public Type getType() {
+    return this.enounterType;
+  }
+  
+  public void setAttack(int attackValue) {
+    this.attackValue = attackValue; 
+  }
+
 }
