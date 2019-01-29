5924eaac3915c79dd0f1445ba9515d2dce9d70f8
Nahuel Paladino <43184105+npala043@users.noreply.github.com>
2019-01-28 19:49:51 -0700

    updated planet

--- CSC Bot/src/main/java/Games/Cosmic/Planet.java
+++ CSC Bot/src/main/java/Games/Cosmic/Planet.java
@@ -1,4 +1,18 @@
 package games.cosmic;
+import java.util.ArrayList;
 
 public class Planet {
+  
+  private ArrayList<Colony> colonyList;
+  private Player.Colour systemColour;
+  
+  public Planet(Player.Colour systemColour) {
+    this.systemColour = systemColour;
+    colonyList = new ArrayList<>();
+  }
+  
+  public void addColony(Colony c) {
+     colonyList.add(c);
+  }
+  
 }
