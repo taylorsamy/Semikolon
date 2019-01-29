513ba96c7246972ecad137fc3b87b83912068e53
Taylor Amy <taylor.amy101@gmail.com>
2019-01-28 19:41:22 -0700

    Added planet class

--- CSC Bot/src/main/java/Games/Cosmic/Game.java
+++ CSC Bot/src/main/java/Games/Cosmic/Game.java
@@ -22,7 +22,7 @@ public class Game {
 
   private void initPlayers() {
     for (int i = 0; i < numPlayers; i++) {
-
+      players.add(new Player(i));
     }
   }
 
--- CSC Bot/src/main/java/Games/Cosmic/Player.java
+++ CSC Bot/src/main/java/Games/Cosmic/Player.java
@@ -7,18 +7,45 @@ import java.util.ArrayList;
 public class Player {
 
     public enum Colour {
-        REG, GREEN, BLUE, YELLOW, PURPLE
+        RED, GREEN, BLUE, YELLOW, PURPLE
     }
 
     private ArrayList<Colony> colonies;
     private ArrayList<Card> hand;
     private ArrayList<Player> allies;
 
+    private int playerNum;
+
     private Colour colour;
 
-    public Player(Colour c) {
-        this.colour = c;
+    public Player(int playerNumber) {
+        playerNum = playerNumber;
+        initPlayer();
+        initColonies();
+    }
 
+    private void initColonies(){
+        //for ()
+    }
+
+    private void initPlayer() {
+        switch (playerNum) {
+            case 0:
+                colour = Colour.RED;
+                break;
+            case 1:
+                colour = Colour.GREEN;
+                break;
+            case 2:
+                colour = Colour.BLUE;
+                break;
+            case 3:
+                colour = Colour.YELLOW;
+                break;
+            case 4:
+                colour = Colour.PURPLE;
+                break;
+        }
     }
 
 
