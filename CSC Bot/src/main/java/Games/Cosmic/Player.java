8001e4219720d0672a0166b454b44bb3811370ac
Taylor Amy <taylor.amy101@gmail.com>
2019-01-28 18:44:25 -0700

    Added allies list

--- CSC Bot/src/main/java/Games/Cosmic/Player.java
+++ CSC Bot/src/main/java/Games/Cosmic/Player.java
@@ -5,7 +5,12 @@ import games.cosmic.cards.Card;
 import java.util.ArrayList;
 
 public class Player {
+
+
     private ArrayList<Colony> colonies;
     private ArrayList<Card> hand;
+    private ArrayList<Player> allies;
+
+
 
 }
