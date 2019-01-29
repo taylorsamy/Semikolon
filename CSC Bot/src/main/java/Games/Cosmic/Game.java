3f7b4e81a047f84d7ec8f7202f1bc35d0d6904c5
Taylor Amy <taylor.amy101@gmail.com>
2019-01-28 18:58:28 -0700

    Started work on Game class

--- CSC Bot/src/main/java/Games/Cosmic/Game.java
+++ CSC Bot/src/main/java/Games/Cosmic/Game.java
@@ -1,17 +1,30 @@
+package games.cosmic;
+
+import java.util.ArrayList;
+
 public class Game {
 
   private enum EncounterPhase {
     REGROUP, DESTINY, LAUNCH, ALLIANCE, PLANNING, REVEAL, RESOLUTION
-  };
+  }
   private EncounterPhase currentPhase;
   
   private int numPlayers;
   private int playerTurn;
+  private ArrayList<Player> players;
   
   public Game(int numPlayers) {
     this.numPlayers = numPlayers;
     //this.playerTurn = 
     this.currentPhase = EncounterPhase.REGROUP;
   }
+
+
+  private void initPlayers() {
+    for (int i = 0; i < numPlayers; i++) {
+
+    }
+  }
+
 
 }
--- CSC Bot/src/main/java/Games/Cosmic/Player.java
+++ CSC Bot/src/main/java/Games/Cosmic/Player.java
@@ -6,10 +6,20 @@ import java.util.ArrayList;
 
 public class Player {
 
+    public enum Colour {
+        REG, GREEN, BLUE, YELLOW, PURPLE
+    }
 
     private ArrayList<Colony> colonies;
     private ArrayList<Card> hand;
     private ArrayList<Player> allies;
+
+    private Colour colour;
+
+    public Player(Colour c) {
+        this.colour = c;
+
+    }
 
 
 
