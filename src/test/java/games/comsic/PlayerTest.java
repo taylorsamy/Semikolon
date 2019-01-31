d770090738ed9594217930122e2eac5e0ee18ff5
Taylor <taylor.amy101@gmail.com>
2019-01-30 20:25:32 -0700

    test

--- src/test/java/games/comsic/PlayerTest.java
+++ src/test/java/games/comsic/PlayerTest.java
@@ -6,7 +6,7 @@ import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-public class PlayerTest {
+public class PlayerTest { // added comment for testing
 
     @Test
     public void testPlayerColours() {
